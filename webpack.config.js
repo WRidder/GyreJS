ff39da23571276b302a0185b0685192ef644ab07
Wilbert van de Ridder <wilbert.ridder@gmail.com>
2015-10-16 05:57:04 +0200

    - Added exported library - Adjusted dependencies - Added webpack build

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, "lib"),
+    filename: 'gyrejs-min.js',
+    libary: "gyrejs",
+    libraryTarget: "umd"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel'
+      }
+    ]
+  },
+  externals: ["immutable", "react"],
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ]
+};
