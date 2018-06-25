514d3900fd0d12eb79f9f9b16a182190963961d0
Wilbert van de Ridder <wilbert.ridder@gmail.com>
2018-06-26 01:54:59 +0900

    Update

--- lib/types/interfaces.d.ts
+++ lib/types/interfaces.d.ts
@@ -1,21 +1,22 @@
 export interface IGyreCommand {
-    id: string;
-    data: any;
+  id: string;
+  data: any;
 }
 export interface IGyreEvent {
-    id: string;
-    data: any;
+  id: string;
+  data: any;
 }
 export interface ICommandHandler {
-    (cmd: IGyreCommand, issue: (evt: IGyreCommand) => void, trigger: (evt: IGyreEvent) => void, getProjectionState: (id: string) => any): void;
+  (cmd: IGyreCommand, issue: (evt: IGyreCommand) => void, trigger: (evt: IGyreEvent) => void,
+   getProjectionState: (id: string) => any): void;
 }
 export interface IGyreOptions {
-    useMultithreading?: boolean;
+  useMultithreading?: boolean;
 }
 export interface IListenerOptions {
-    id?: string;
-    priority?: number;
+  id?: string;
+  priority?: number;
 }
 export interface IReducer {
-    (state: object, event: IGyreEvent): any | undefined;
+  (state: object, event: IGyreEvent): any | undefined;
 }
