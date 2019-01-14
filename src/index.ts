69fb9d39017f8030762d6fca8ae3d9bc6a929203
Wilbert van de Ridder <wilbert.ridder@gmail.com>
2019-01-14 20:41:59 +0100

    Added debug statement

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,8 @@ import { IGyreCommand, IGyreEvent, IListenerOptions } from './interfaces';
 import { Projection } from './projection';
 import { ECManager } from './ecmanager';
 
+/// #if DEBUG
+console.log('[GyreJS] You running a debug build.');
+/// #endif
+
 export { Gyre, ECManager, Projection, IGyreCommand, IGyreEvent, IListenerOptions };
