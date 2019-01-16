4e58913440ad41cb44f3f00a87eefe2761501032
Wilbert van de Ridder <wilbert.ridder@gmail.com>
2019-01-16 23:00:26 +0100

    Remove types from repo. Will be generated automatically upon build.

--- types/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { Gyre } from './gyre';
-import { IGyreCommand, IGyreEvent, IListenerOptions } from './interfaces';
-import { Projection } from './projection';
-import { ECManager } from './ecmanager';
-export { Gyre, ECManager, Projection, IGyreCommand, IGyreEvent, IListenerOptions };
--- types/task.d.ts
+++
@@ -1,5 +0,0 @@
-import { Projection } from './projection';
-import { IGyreEvent } from './interfaces';
-export declare class Task extends Projection {
-    constructor(state: object, reducer: (state: object, event: IGyreEvent) => object | undefined);
-}
