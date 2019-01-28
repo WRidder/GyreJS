d336dff8bf6014ae22c95b8045487370bf6173ab
Wilbert van de Ridder <wilbert.ridder@gmail.com>
2019-01-28 21:08:16 +0100

    Fix running tests twice.

--- tests/db.ts
+++
@@ -1,7 +0,0 @@
-// setup tests
-const dexie = require('dexie');
-dexie.dependencies.indexedDB = require('fake-indexeddb');
-dexie.dependencies.IDBKeyRange = require('fake-indexeddb/lib/FDBKeyRange');
-
-const databases = await Dexie.getDatabaseNames();
-console.log(databases);
